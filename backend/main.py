6de68c1a43723ec5121f0598c93ec13e71168e40
mingchanoknan <63070027@kmitl.ac.th>
2023-03-26 01:47:52 +0700

    add cors policy

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,18 @@ import base64
 
 app = FastAPI()
 
+# cors policy
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 class ImageRequest(BaseModel):
     image: str
     name: str
